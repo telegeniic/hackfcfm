cc9e23e5728900b720ff452c9427e5bb934ea882
OTobias09 <55625993+OTobias09@users.noreply.github.com>
2019-10-27 05:17:21 -0600

    html con vinculos

--- app/routes.py
+++ app/routes.py
@@ -7,15 +7,6 @@ from app.forms import LoginForm
 def home():
     return render_template('index.html')
 
-@app.route('/signin')
-def registrar():
-    return '<h1>Pagina de registro</h1>'
-
-@app.route('/login')
-def inicarSecion():
-    form = LoginForm()
-    return render_template('login.html', form=form)
-
 @app.route('/agenda')
 def dropdown():
     Entradas = ['7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm']
