c7e4e412d953a1434ae3206866de26987e82178a
Telegenic <martinguajardo20@gmail.com>
2019-10-27 08:56:10 -0600

    Finaliza el demoday

--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,8 @@ segunda = [clases[1], clases[1], clases[1], clases[1], clases[1]]
 tercera = [clases[2], clases[2], clases[2], clases[2], clases[2]]
 cuarta = [clases[3], clases[3], clases[3], clases[3], clases[3]]
 quinta = [clases[4], clases[4], clases[4], clases[4], clases[4]]
-sexta = [clases[5], clases[5], clases[5], clases[5], clases[5]]
-septima = [clases[6], clases[6], clases[6], clases[6], clases[6]]
+sexta = [clases[5], clases[6]]
+septima = [clases[5], clases[6]]
 Entradas = ['700', '800', '900', '1000', '1100', '1200', '1300']
 Salidas = ['800', '900', '1000', '1100', '1200', '1300', '1400', '1500']
 
@@ -26,5 +26,10 @@ def dropdown():
     return render_template('agendapre.html', Entradas=Entradas, Salidas=Salidas)
 
 @app.route('/submitagenda', methods=['POST'])
+
 def submited():
-    return render_template('agenda.html', Entradas=Entradas, Salidas=Salidas)
+    entrada = request.form.get("Entradas")
+    salida = request.form.get("Salidas")
+    entrada = int(entrada)
+    salida = int(salida)
+    return render_template('agenda.html',entrada=entrada, salida=salida, Entradas=Entradas, Salidas=Salidas, primera=primera,segunda=segunda,tercera=tercera,cuarta=cuarta,quinta=quinta,sexta=sexta,septima=septima)
