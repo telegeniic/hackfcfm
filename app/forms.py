53fa18b11c32a732972f7dc7fe18205c4354790b
Telegenic <martinguajardo20@gmail.com>
2019-10-26 20:55:35 -0500

    Se termino el form de inicio de sesion

--- app/forms.py
+++ app/forms.py
@@ -2,3 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+class LoginForm(FlaskForm):
+    correo = StringField('Correo', validators=[DataRequired()])
+    password = PasswordField('Contrasenna', validators=[DataRequired()])
+    recuerdame = BooleanField('Recuerdame')
+    subir = SubmitField('Iniciar Secion')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 from flask import render_template
-from app import app 
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def home():
     return render_template('inicio.html')
 
-@app.route('/registro')
+@app.route('/signin')
 def registrar():
     return '<h1>Pagina de registro</h1>'
+
+@app.route('/login')
+def inicarSecion():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/agenda')
+def agenda():
+    return render_template('agenda.html')
